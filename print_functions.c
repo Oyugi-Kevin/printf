5f23a33cb9a57792cfc226ac216b9788427a8cd3
Oyugi-Kevin <mr.kevin.oyugi@gmail.com>
2023-01-26 13:51:09 -0800

    prints integer

--- print_functions.c
+++ print_functions.c
@@ -20,3 +20,23 @@ int print_char(va_list ap, params_t *params)
 		sum += _putchar(ch);
 	return (sum);
 }
+
+**
+ * print_int - prints integer
+ * @ap: argument pointer
+ * @params: the parameters struct
+ *
+ * Return: number chars printed
+ */
+int print_int(va_list ap, params_t *params)
+{
+	long l;
+
+	if (params->l_modifier)
+		l = va_arg(ap, long);
+	else if (params->h_modifier)
+		l = (short int)va_arg(ap, int);
+	else
+		l = (int)va_arg(ap, int);
+	return (print_number(convert(l, 10, 0, params), params));
+}
