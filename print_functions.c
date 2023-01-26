0fa37603d2edf067244ec30ba5c9b1bb235bb4ae
Oyugi-Kevin <mr.kevin.oyugi@gmail.com>
2023-01-26 13:54:27 -0800

    prints string 2

--- print_functions.c
+++ print_functions.c
@@ -82,3 +82,17 @@ int print_string(va_list ap, params_t *params)
 	}
 	return (sum);
 }
+
+/**
+ * print_percent - prints string
+ * @ap: argument pointer
+ * @params: the parameters struct
+ *
+ * Return: number chars printed
+ */
+int print_percent(va_list ap, params_t *params)
+{
+	(void)ap;
+	(void)params;
+	return (_putchar('%'));
+}
