8cf56a2e22a91ec91b5e48f61aa98346b820f94d
Oyugi-Kevin <mr.kevin.oyugi@gmail.com>
2023-01-26 14:33:28 -0800

    prints a char's ascii value in uppercase hex

--- print_char.c
+++ print_char.c
@@ -34,3 +34,28 @@ int print_s(va_list s)
 	}
 	return (count);
 }
+
+/**
+ * main: hex_print - prints a char's ascii value in uppercase hex
+ * definition:
+ * @c: char to print
+ *
+ * Return: number of chars printed (always 2)
+ */
+static int hex_print(char c)
+{
+	int count;
+	char diff = 'A' - ':';
+	char d[2];
+
+	d[0] = c / 16;
+	d[1] = c % 16;
+	for (count = 0; count < 2; count++)
+	{
+		if (d[count] >= 10)
+			_putchar('0' + diff + d[count]);
+		else
+			_putchar('0' + d[count]);
+	}
+	return (count);
+}
