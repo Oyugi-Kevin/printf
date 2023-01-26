0420c7fdf3fa0e43f411a3882825d9260d18ab32
Oyugi-Kevin <mr.kevin.oyugi@gmail.com>
2023-01-26 14:37:48 -0800

    takes an unsigned int and prints it in lowercase hex notation

--- print_hex.c
+++ print_hex.c
@@ -40,3 +40,27 @@ int print_hex(unsigned int n, unsigned int c)
 	}
 	return (count);
 }
+
+/**
+ * print_x - takes an unsigned int and prints it in lowercase hex notation
+ * description:
+ * @x: unsigned int to print
+ *
+ * Return: number of digits printed
+ */
+int print_x(va_list x)
+{
+	return (print_hex(va_arg(x, unsigned int), 0));
+}
+
+/**
+ * print_X - takes am unsigned int and prints it in uppercase hex notation
+ * description:
+ * @X: unsigned int to print
+ *
+ * Return: number of digits printed
+ */
+int print_X(va_list X)
+{
+	return (print_hex(va_arg(X, unsigned int), 1));
+}
