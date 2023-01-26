3a78dccbc2f7288e15bd9628d283444ac7613700
Oyugi-Kevin <mr.kevin.oyugi@gmail.com>
2023-01-26 14:07:07 -0800

    finds the flag func

--- specifier.c
+++ specifier.c
@@ -54,3 +54,35 @@ int get_print_func(char *s, va_list ap, params_t *params)
 		return (f(ap, params));
 	return (0);
 }
+
+/**
+ * get_flag - finds the flag func
+ * @s: the format string
+ * @params: the parameters struct
+ *
+ * Return: if flag was valid
+ */
+int get_flag(char *s, params_t *params)
+{
+	int i = 0;
+
+	switch (*s)
+	{
+		case '+':
+			i = params->plus_flag = 1;
+			break;
+		case ' ':
+			i = params->space_flag = 1;
+			break;
+		case '#':
+			i = params->hashtag_flag = 1;
+			break;
+		case '-':
+			i = params->minus_flag = 1;
+			break;
+		case '0':
+			i = params->zero_flag = 1;
+			break;
+	}
+	return (i);
+}
